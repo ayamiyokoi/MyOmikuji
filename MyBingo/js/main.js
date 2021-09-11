6398524101e015c7036da4a05ebd3cb0280a6737
AyamiYokoi <didufd63hff@gmail.com>
2021-09-11 14:51:11 +0900

    [Add]ビンゴ機能

--- MyBingo/js/main.js
+++ MyBingo/js/main.js
@@ -1,21 +1,41 @@
 'use strict';
 
 {
-  // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  const source = [];
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+  
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    return column;
   }
 
-  const b = [];
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
   }
 
-  console.log(b);
+  function renderBingo(columns) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
 }
